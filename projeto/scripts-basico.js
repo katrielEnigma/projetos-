d24b7d58e4aa2d83d3bf9d33e2ea8a2cd2d40a69
katrielEnigma <168040296+katrielEnigma@users.noreply.github.com>
2024-05-08 20:32:20 -0300

    Add files via upload

---
+++ projeto/scripts-basico.js
@@ -0,0 +1,22 @@
+//O vento DOM é acionado quando todo HTML foi completamente carregado
+
+document.addEventListener('DOMContentLoaded', function (){
+    alert(123);
+    
+    //tipos de dados
+    //string: sequencia de caracter 
+    //Number: números inteiros ou pontos flutuantes (10.4)
+    //boolean: Retorna dois valores, true ou false
+    //object: estrturas de dados que podem amarzenar múltiplos valores
+    //Null: quando está vazio
+    //array: objeto especial ordenado 
+    //bloco de código reutilizável
+    
+    //const é usado pra declarar uma váriavel cujo o valor é permanente
+    const B = 0;
+    
+
+    //è usado para de declarar váriaveis que os valores podem ser retribuidos
+    let = 0;
+
+})
